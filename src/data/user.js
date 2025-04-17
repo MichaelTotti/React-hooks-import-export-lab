677261e996155b514ed04995ed38d2bc6f1cc866
Michael <michael.onyango2@student.moringaschool.com>
2025-04-17 11:08:36 +0300

    1

--- src/data/user.js
+++ src/data/user.js
@@ -3,4 +3,9 @@ const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
+const user = [username, city, image]; // array destructuring
+
+export default user; // export default username;
+// export default city;
+// export default image;
 // export all three variables as named exports
